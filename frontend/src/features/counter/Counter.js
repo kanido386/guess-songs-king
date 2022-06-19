d753ce734784df6ee22e5fbd386602b85b847c76
kanido386 <kanido386@gmail.com>
2022-06-19 21:58:04 +0800

    style: Use eslint and prettier to format the code

--- frontend/src/features/counter/Counter.js
+++ frontend/src/features/counter/Counter.js
@@ -21,18 +21,18 @@ export function Counter() {
     <div>
       <div className={styles.row}>
         <button
+          type="button"
           className={styles.button}
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+          onClick={() => dispatch(decrement())}>
           -
         </button>
         <span className={styles.value}>{count}</span>
         <button
+          type="button"
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+          onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
@@ -44,21 +44,21 @@ export function Counter() {
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
+          type="button"
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
+          onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
         </button>
         <button
+          type="button"
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
+          onClick={() => dispatch(incrementAsync(incrementValue))}>
           Add Async
         </button>
         <button
+          type="button"
           className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}>
           Add If Odd
         </button>
       </div>
